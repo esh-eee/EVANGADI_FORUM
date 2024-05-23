8803294b917e642b8dde6ae8a015f02bedede69a
Eshetu Assefa <assefaeshetu1221@gmail.com>
2024-05-23 23:00:10 +0300

    deploying MySQL

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,12 +1,21 @@
 const mysql2 = require("mysql2");
- 
+ const fs = require("fs");
+ const path = require("path");
+
+const sslCert = fs.readFileSync(path.resolve(__dirname, "certificate.pem"));
+
+
 // use to connect backend code with the specefic database
 const dbconnection = mysql2.createPool({
   user: process.env.USER,
+  port: process.env.PORT,
   database: process.env.DATABASE,
-  host: "localhost",
+  host: process.env.HOST,
   password: process.env.PASSWORD,
   connectionLimit: 10,
+  ssl: {
+    ca: sslCert,
+  },
 });
 
 // dbconnection.execute("select 'test' ", (err, result) => {
